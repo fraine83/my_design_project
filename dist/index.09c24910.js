5719d0c97327985201cd6a2b8ec7b626d781d55c
fraine83 <fraiune14@gmail.com>
2025-02-22 01:40:19 +0800

    adding dist

---
+++ dist/index.09c24910.js
@@ -0,0 +1,42 @@
+var swiper = new Swiper('.product-slider', {
+    slidesPerView: 1,
+    spaceBetween: 8,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    //   navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 18
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 18
+        }
+    }
+});
+var swiper = new Swiper(".reviewSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 8,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 16
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 28
+        }
+    }
+});
+
+//# sourceMappingURL=index.09c24910.js.map
